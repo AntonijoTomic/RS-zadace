69f4fa92959cb9024664705389b57b51dc3c7061
AntonijoTomic <antonijo.tomic12@gmail.com>
2024-11-22 15:03:47 +0100

    ispravak

---
+++ Zadatak 1/Zadatak 1Lambda izrazi.py
@@ -0,0 +1,29 @@
+#Zad1
+
+kvadriraj = lambda x: x ** 2
+
+print(kvadriraj(2))
+
+#Zad2
+
+zbroji_pa_kvadriraj = lambda a,b: (a+b) **2
+
+print(zbroji_pa_kvadriraj(3,3))
+
+#Zad3
+
+kvadriraj_duljinu = lambda niz: len(niz) ** 2
+
+print(kvadriraj_duljinu([1,2,3,4,5]))
+
+#zad4
+
+pomnozi_i_potenciraj = lambda x,y: (y * 5) ** x 
+
+print(pomnozi_i_potenciraj(2,3))
+
+#zad5
+
+paran_broj = lambda x: True if x%2 == 0 else None
+print(paran_broj(21))
+
